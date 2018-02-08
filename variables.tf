409cbf36f7173367a90aca1a7c2614dba89cf7d2
Avishek Das <avisdas9@in.ibm.com>
2018-02-08 12:13:06 +0530

    initial

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "public_key_path" {
+  description = "public key path"
+  default = "C:/Workspace/Terraform/id_rsa_pub"
+}
+
+variable "key_name" {
+  description = "Desired name of AWS key pair"
+  default = "key_pair"
+}
+
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "us-west-2"
+}
+
+# Ubuntu Precise 12.04 LTS (x64)
+variable "aws_amis" {
+  default = {
+    eu-west-1 = "ami-674cbc1e"
+    us-east-1 = "ami-1d4e7a66"
+    us-west-1 = "ami-969ab1f6"
+    us-west-2 = "ami-8803e0f0"
+  }
+}
