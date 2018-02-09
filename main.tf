0c988807e0e4a060b462ac4f8068ffff782ca3cb
root <root@ip-172-31-37-172.eu-central-1.compute.internal>
2018-02-09 10:27:46 +0000

    working in aws

--- main.tf
+++ main.tf
@@ -81,4 +81,47 @@ resource "aws_security_group" "default" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+resource "aws_key_pair" "auth" {
+  key_name   = "${var.key_name}"
+  public_key = "${file(var.public_key_path)}"
+}
 
+resource "aws_instance" "web" {
+  # The connection block tells our provisioner how to
+  # communicate with the resource (instance)
+  connection {
+    # The default username for our AMI
+    type = "ssh"
+    user = "ec2-user"
+    private_key = "${file("/root/terraformkey")}"
+    # The connection will use the local SSH agent for authentication.
+  }
+
+  instance_type = "t2.micro"
+
+  # Lookup the correct AMI based on the region
+  # we specified
+  ami = "${lookup(var.aws_amis, var.aws_region)}"
+
+  # The name of our SSH keypair we created above.
+  key_name = "${aws_key_pair.auth.id}"
+
+  # Our Security group to allow HTTP and SSH access
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+
+  # We're going to launch into the same subnet as our ELB. In a production
+  # environment it's more common to have a separate private subnet for
+  # backend instances.
+  subnet_id = "${aws_subnet.default.id}"
+
+  # We run a remote provisioner on the instance after creating it.
+  # In this case, we just install nginx and start it. By default,
+  # this should be on port 80
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get -y update",
+      "sudo apt-get -y install nginx",
+      "sudo service nginx start",
+    ]
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,21 +1,21 @@
 variable "public_key_path" {
   description = "public key path"
-  default = "/home/ec2-user/terraform/developerkeypair.pub"
+  default = "/root/terraformkey.pub"
 }
 
 variable "key_name" {
   description = "Desired name of AWS key pair"
-  default = "developerkeypair"
+  default = "developerkeypair1"
 }
 
 variable "aws_region" {
   description = "AWS region to launch servers."
-  default     = "eu-central-1b"
+  default     = "eu-central-1"
 }
 
 # Ubuntu Precise 12.04 LTS (x64)
 variable "aws_amis" {
   default = {
-    eu-central-1b = "ami-5652ce39"
+    eu-central-1 = "ami-5652ce39"
   }
 }
